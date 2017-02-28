2cd33d88fbec4fa12beb691dddf137e51cdffe54
Yang Wang <yangwangx@gmail.com>
2017-02-28 16:01:00 -0500

    Silence setup function when there is no error

--- ml_setup.m
+++ ml_setup.m
@@ -1,3 +1,4 @@
+function ml_setup(varargin)
 % Setup the environments
 % By: Minh Hoai Nguyen (minhhoai@cs.stonybrook.edu)
 % Created: 12-Jul-2015
@@ -8,10 +9,9 @@ libRootPath = pathOfThisFile(1:end-length('ml_setup'));
 
 libs = {'.', 'Classifier', 'Misc', 'ComponentAnalysis', 'Video', 'Kmeans'};
 
-fprintf('Adding the following directories to the Matlab paths\n');
+% Add directories to Matlab paths
 for i=1:length(libs)
     libPath = sprintf('%s%s', libRootPath, libs{i});
-    fprintf('  %s\n', libPath);
     addpath(libPath);
 end;
 
@@ -19,7 +19,6 @@ end;
 isVideoWork = 1;
 try 
     a = vl_version();
-    fprintf('Good. VL_FEAT library version %s found\n', a);
 catch
     fprintf('VL_FEAT library not found\n');
     fprintf('  Some functions in %sVideo might not work', libRootPath);
@@ -27,15 +26,12 @@ catch
 end;
 
 ffmpegBin = ML_Ffmpeg.getFfmpegBin();
-[retCode, retOutput] = system(ffmpegBin);
+[retCode, retOutput] = system(sprintf('%s -version', ffmpegBin));
 if retCode ~=0
     fprintf('FFMPEG library not installed or incorrect ffmpeg binary\n');
     fprintf('  Some functions in %sVideo will not work\n', libRootPath);
     fprintf('  Install FFMPEG and set the correct paths in %sVideo/ML_Ffmpeg.m\n', libRootPath);
     isVideoWork = 0;
-else
-    fprintf('Good. FFmpeg binary seems to work well');
 end
     
 [version, date] = ml_version();
-fprintf('Finish setting MLHfuncs, version %s, date %s\n', version, date);
